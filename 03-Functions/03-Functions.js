791d527caad4b429668ebf8b7ff625c073d89052
Elizabeth Gallardo <pri00gallardo@gmail.com>
2022-04-05 19:04:58 -0300

    first

--- 03-Functions/03-Functions.js
+++ 03-Functions/03-Functions.js
@@ -1,8 +1,14 @@
 function timeConversion(time){
-// dada la hora en formato hora AM/PM , convertir a la hora militar (24-horas).
-// Recordar: las 12:00:00AM en un reloj de 12 horas son las 00:00:00 en un reloj de 24 horas
-//las 12:00:00PM en un reloj de 12 horas son las 12:00:00 en un reloj de 24 horas 
+    // dada la hora en formato hora AM/PM , convertir a la hora militar (24-horas).
+    // Recordar: las 12:00:00AM en un reloj de 12 horas son las 00:00:00 en un reloj de 24 horas
+    //las 12:00:00PM en un reloj de 12 horas son las 12:00:00 en un reloj de 24 horas 
 
+    let AMPM = time.slice(8);
+    let hora = time.slice(0, 2);
+    let min = time.slice(3, 5);
+    let seg = time.slice(6, 8);
+
+    //if(AMPM === "PM" && hora === "12" || hora < 12)
 }
 
 module.exports = {
--- 04-Recursion/04-recursion.js
+++ 04-Recursion/04-recursion.js
@@ -4,7 +4,9 @@
 // producto([7, 2, 5]) devuelve 70
 const producto = function (array) {
   //escribe aqui tu codigo
-  
+  if(array.length === 0) return 1;
+  return array.shift() * producto(array);
+
 };
 
 // Dado un objeto con objetos anidados utilizar la recursión para crear una función
@@ -24,5 +26,12 @@ const producto = function (array) {
 // }
 const isThere = function (obj, value) {
   //escribe aqui tu codigo
+  for (const key in obj) {
+    if (obj[key] === value) return true;
+    if(typeof obj[key] === 'object'){
+      if(isThere(obj[key], value)) return true;
+    }
+  }
+  return false;
 };
 module.exports = { producto, isThere };
